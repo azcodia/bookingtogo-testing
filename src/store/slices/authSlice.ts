28b614b8946fc28df5fde0461bbefd1e69dc0374
Rahmad Iqbal <azcodia38@gmail.com>
2025-10-03 07:39:06 +0700

    feat(redux): configure store, auth slice, and persistor

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -2,12 +2,10 @@ import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "../store";
 import type { AuthState } from "../../interface/auth.interface.ts";
 
-// Initial state
 const initialState: AuthState = {
   isLoggedin: false,
 };
 
-// Create the slice
 const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -18,11 +16,8 @@ const authSlice = createSlice({
   },
 });
 
-// Export actions
 export const { setLoginStatus } = authSlice.actions;
 
-// Export selector (you can add more selectors if needed)
 export const selectIsLoggedIn = (state: RootState) => state.auth.isLoggedin;
 
-// Export reducer
 export default authSlice.reducer;
